82212606a6fdfbd473d522a0890d9766635fc06b
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2020-02-16 23:16:27 +0100

    Added download of vimrc file

--- mac.sh
+++ mac.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 pip3 install pynvim
+
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/mvmaasakkers/workspace/master/vimrc
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,8 @@
 
 sudo apt install python3-pip
 
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/mvmaasakkers/workspace/master/vimrc
+
 pip3 install pynvim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
